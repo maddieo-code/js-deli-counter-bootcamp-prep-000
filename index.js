5b9e06ef88802c29ae824f8648504deedbc4858f
Maddie O'Meara <maddie.omeara@gmail.com>
2017-09-29 17:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${katzDeliLine.shift()}.;`
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
 
 function currentLine(katzDeliLine) {
-  var line = "The line is currently: "
+  var line = "The line is currently:  "
   if (katzDeliLine.length === 0) {
     line = "The line is currently empty."
   } else {
