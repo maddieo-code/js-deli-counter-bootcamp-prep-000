cdcc967a797354c953d11bfd30f5285d1bc7ad2b
Maddie O'Meara <maddie.omeara@gmail.com>
2017-09-29 17:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    katzDeliLine.shift();
-    return `Currently serving ${katzDeliLine[0]}.`
+    return `Currently serving ${katzDeliLine.shift()};.`
   }
 }
