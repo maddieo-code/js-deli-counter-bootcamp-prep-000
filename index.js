5f5601810bee9a27cbb85f27a1656bea3ee1223a
Maddie O'Meara <maddie.omeara@gmail.com>
2017-09-29 17:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     line = "The line is currently empty."
   } else {
-    for (var i = 0; i < katzDeliLine.length; i++) {
+    for (var i = 0; i < katzDeliLine.length - 1; i++) {
       line = `${line} ${i+1}. ${katzDeliLine[i]},`;
     }
+      line = `${line}` ${katzDeliLine.length}. ${katzDeliLine[katzDeliLine.length-1]}`;
   }
   return line;
 }
