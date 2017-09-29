465d54a61bc0a349c6ee0ace4468a72670a862e8
Maddie O'Meara <maddie.omeara@gmail.com>
2017-09-29 17:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person);
   return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return `Now serving {katzDeliLine[0]}.`
+  }
+}
